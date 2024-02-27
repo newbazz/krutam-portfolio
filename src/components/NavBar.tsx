95d637a7a336b34917efbe10cb5d27e19feec8bf
suraj-pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-02-27 11:56:04 +0530

    fix hydration error

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,16 +3,14 @@ import React from 'react'
 import Link from 'next/link';
 import Logo from './Logo';
 
-//const CustomLink = ({href, title, className=""}) =>
-
 function NavBar() {
     return (
       <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
         <nav>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/articles">Articles</Link>
+          <Link href="/" title="Home" className='mr-4'>Home</Link>
+          <Link href="/about" className='mx-4' >About</Link>
+          <Link href="/projects" className='mx-4'>Projects</Link>
+          <Link href="/articles" className='mx-2'>Articles</Link>
         </nav>
       
         <nav>
