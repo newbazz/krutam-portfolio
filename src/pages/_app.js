43bfbc0e399856af0df5536c7195d1c6a1c80686
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-03-03 12:13:16 +0530

    add favicon, change website name & description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,7 @@ export default function MyApp({ Component, pageProps }) {
     
     <Head>
       <meta name = "viewport" content="width=device-width, initial-scale=1" />
-      <link rel = "icon" href = "/favicon.ico"/>
+      <link rel = "icon" href = "/favicon.png"/>
     </Head>
 
     <main className={`${inter.variable} font-sans bg-light w-full min-h-screen`}>
