d31c71bf4d3182a1ad8e80684653e9e06382bc7e
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-03-24 21:55:19 +0530

    made about page responsive

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,17 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
-import { CircularText,} from './Icons'
+import Link from "next/link";
+import React from "react";
+import { CircularText } from "./Icons";
 
 const ContactMe = () => {
   return (
-    <div className='fixed left-4 bottom-4
+    <div
+      className="fixed left-4 bottom-4
     flex item-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto
     md:absolute sm:right-0
-    '>
-     <div className='w-48 h-auto flex items-center justify-center relative md:w-24'>
-      <CircularText className={"fill-dark dark:fill-light animate-spin-slow "} />
+    "
+    >
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
+        <CircularText
+          className={"fill-dark dark:fill-light animate-spin-slow "}
+        />
 
-      <Link href="mailto:krutamhathi14@gmail.com" className='flex items-center justify-center
+        <Link
+          href="mailto:krutamhathi14@gmail.com"
+          className="flex items-center justify-center
       absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light
       shadow-md border border-solid border-dark w-20 h-20 rounded-full
       font-semibold text-5xl hover:bg-light hover:text-dark
@@ -19,12 +25,15 @@ const ContactMe = () => {
       dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
       hover:dark:border-light
 
-      md:w-12 md:h-12 md:text-[30px]
+      md:w-12 md:h-12 md:text-[25px]
       
-      '>🤝</Link>
-     </div>
+      "
+        >
+          🤝
+        </Link>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,49 +1,59 @@
-import React from 'react'
-import {motion} from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
-const Skill = ({name, x, y}) => {
-
-    return(
-      <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
+const Skill = ({ name, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light
       py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light
-      '
-      whileHover={{scale:1.05}}
-      initial={{x:0,y:0}}
-      whileInView={{x:x, y:y, transition: {duration:1.5}}}
-      transition = {{duration:1.5}}
-      >
+      lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent
+      xs:text-dark xs:dark:text-light xs:font-bold
+      "
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
+      transition={{ duration: 1.5 }}
+    >
       {name}
-      </motion.div>
-    )
-}
+    </motion.div>
+  );
+};
 
 const Skills = () => {
   return (
     <>
-      <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
-      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark'>
-
-          <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light
+      <h2 className="font-bold text-7xl mt-[150px] w-full text-center md:text-6xl md:mt-32 sm:mt-12 lg:!text-7xl sm:!text-6xl xs:!text-4xl">
+        Skills
+      </h2>
+      <div
+        className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
+      lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
+      lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+      md:bg-circularLightMd md:dark:bg-circularDarkMd
+      sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+      "
+      >
+        <motion.div
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light
           p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light
-          '
-          whileHover={{scale:1.05}}
-          >
-            Web
-          </motion.div>
+          lg:p-6 md:p-4 xs:text-xs xs:p-2
+          "
+          whileHover={{ scale: 1.05 }}
+        >
+          Web
+        </motion.div>
 
-          <Skill name = "Azure" x="-25vw" y="2vw" />
-          <Skill name = "C#" x="-5vw" y="-10vw" />
-          <Skill name = "Python" x="20vw" y="6vw" />
-          <Skill name = "CPP" x="0vw" y="12vw" />
-          <Skill name = "ML" x="-20vw" y="-15vw" />
-          <Skill name = "Mathematics" x="15vw" y="-12vw" />
-          <Skill name = "SQL" x="32vw" y="-5vw" />
-          <Skill name = "Cosmos" x="0vw" y="-20vw" />
-         {/* <Skill name = "SQL" x="-25vw" y="18vw" />
-          <Skill name = "Tailwind CSS" x="18vw" y="18vw" />  */}
+        <Skill name="Azure" x="-20vw" y="0vw" />
+        <Skill name="C#" x="10vw" y="-10vw" />
+        <Skill name="Python" x="18vw" y="6vw" />
+        <Skill name="CPP" x="-8vw" y="-10vw" />
+        <Skill name="ML" x="-20vw" y="-15vw" />
+        <Skill name="SQL" x="22vw" y="-10vw" />
+        <Skill name="Cosmos" x="0vw" y="-20vw" />
+        <Skill name="Mathematics" x="-5 vw" y="12vw" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
